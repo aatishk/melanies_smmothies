26034cd59a9e09aa983951a0de6bfa2cc2a1e38b
Aatish Kumar <aatishk@live.com>
2024-04-06 07:40:40 +0200

    Update streamlit_app.py
    clean up and add search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,11 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# option = st.selectbox(
-#     'What is your favorite fruit?',
-#     ('Banana', 'Strawberry', 'Peaches'))
-
-# st.write('Your favorite fruit is:', option)
-
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data = my_dataframe, use_container_width = True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -30,9 +25,6 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
@@ -45,9 +37,6 @@ if ingredients_list:
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
     
     time_to_insert = st.button('Submit Order')
     
